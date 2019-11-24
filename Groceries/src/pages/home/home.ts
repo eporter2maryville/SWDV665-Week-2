9c7ba1a1a231b49484c78178fc8c6f9c1facb9e0
Brent Porter <eporter2@live.maryville.edu>
2019-11-24 13:06:48 -0700

    moved UI elements to service

--- Groceries/src/pages/home/home.ts
+++ Groceries/src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
 
 
 @Component({
@@ -14,7 +15,7 @@ export class HomePage {
   title = "Grocery";
 
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider, public InputDialogService: InputDialogServiceProvider) {
 
   }
 
@@ -40,81 +41,13 @@ export class HomePage {
       duration: 3000
     });
     toast.present(),
-    this.showEditItemPrompt(item, index);
+      this.InputDialogService.showPrompt(item, index);
 
   }
 
   addItem() {
     console.log("Adding an item to list");
-    this.showAddItemPrompt();
-  }
-
-  showAddItemPrompt() {
-    const prompt = this.alertCtrl.create({
-      title: "Add Item to List",
-      message: "Please enter the item name and quantity you would like adde to your grocery list",
-      inputs: [
-        {
-          name: 'name',
-          placeholder: 'Bananas'
-        },
-        {
-          name: 'quantity',
-          placeholder: '1'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log("Cancel Clicked");
-          }
-        },
-        {
-          text: 'Save',
-          handler: item => {
-            console.log('Saved Clicked', item);
-            this.dataService.addItem(item);
-          }
-        }
-      ]
-    });
-    prompt.present();
-  }
-
-  showEditItemPrompt(item, index) {
-    const prompt = this.alertCtrl.create({
-      title: "Edit the selected item",
-      message: "Please update the name or quantitiy of the item in your grocery list",
-      inputs: [
-        {
-          name: 'name',
-          placeholder: 'Bananas',
-          value:item.name
-        },
-        {
-          name: 'quantity',
-          placeholder: '1',
-          value: item.quantity
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log("Cancel Clicked");
-          }
-        },
-        {
-          text: 'Save',
-          handler: item => {
-            console.log('Saved Clicked', item);
-            this.dataService.editItem(item, index);
-          }
-        }
-      ]
-    });
-    prompt.present();
+    this.InputDialogService.showPrompt();
   }
 
 }
--- Groceries/src/providers/input-dialog-service/input-dialog-service.ts
+++ Groceries/src/providers/input-dialog-service/input-dialog-service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+/*import { HttpClient } from '@angular/common/http';*/
 import { Injectable } from '@angular/core';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { AlertController } from 'ionic-angular';
 
 /*
   Generated class for the InputDialogServiceProvider provider.
@@ -10,8 +12,47 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class InputDialogServiceProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(public dataService: GroceriesServiceProvider, public alertCtrl: AlertController) {
     console.log('Hello InputDialogServiceProvider Provider');
   }
 
+  showPrompt(item?, index?) {
+    const prompt = this.alertCtrl.create({
+      title: item ? "Edit Item" : "Add Item",
+      message: item ? "Please update the name or quantitiy of the item in your grocery list" : "Please enter the item name and quantity you would like add to your grocery list",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Bananas',
+          value: item ? item.name : null
+        },
+        {
+          name: 'quantity',
+          placeholder: '1',
+          value: item ? item.quantity : null
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log("Cancel Clicked");
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved Clicked', item);
+            if (index !== undefined) {
+              this.dataService.editItem(item, index);
+            }
+            else {
+              this.dataService.addItem(item);
+            }
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
