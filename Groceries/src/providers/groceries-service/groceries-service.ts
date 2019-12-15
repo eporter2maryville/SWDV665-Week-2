f22519c64d8daeab156417bd7ebe2ad91761681c
eporter2maryville <39113621+eporter2maryville@users.noreply.github.com>
2019-12-15 15:49:33 -0700

    Update groceries-service.ts

--- Groceries/src/providers/groceries-service/groceries-service.ts
+++ Groceries/src/providers/groceries-service/groceries-service.ts
@@ -20,7 +20,7 @@ export class GroceriesServiceProvider {
 
   private dataChangeSubject: Subject<boolean>;
 
-  baseURL =  "http://localhost:8080";
+  baseURL =  "https://groveries-server-demo.herokuapp.com";
   
   constructor(public http:HttpClient) {
     console.log('Hello GroceriesServiceProvider Provider');
